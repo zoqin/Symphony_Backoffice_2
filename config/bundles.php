7cc81fb159d1dab9f967559bfe4ec5936c61e9e7
zoqin <zoqinrobinpk@hotmail.fr>
2026-02-22 16:08:12 +0100

    Prérequis pour redirection au login et composer require --dev orm-fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class SecurityController extends AbstractController
 {
@@ -28,5 +30,23 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    use TargetPathTrait;
+    #[Route(path: '/redirect-after-login', name: 'app_redirect_after_login')]
+    public function redirectAfterLogin(Request $request): Response
+    {
+        $targetPath = $this->getTargetPath($request->getSession(), 'main');
+
+        if($targetPath) {
+            $this->removeTargetPath($request->getSession(), 'main');
+            return $this->redirect($targetPath);
+        }
+
+        if($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->redirectToRoute('app_home');
     }
 }
