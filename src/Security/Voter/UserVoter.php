1e33453f2969e4a86b77062cb60f3176253e93bb
zoqin <zoqinrobinpk@hotmail.fr>
2026-02-23 15:38:19 +0100

    make:crud User, make:voter UserVoter, configuration du voter pour limiter l'accès au crud User à ROLE_ADMIN

---
+++ src/Security/Voter/UserVoter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class UserVoter extends Voter
+{
+    public const MANAGE = 'USER_MANAGE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::MANAGE])
+            && ($subject === null || $subject instanceof User);
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        return match($attribute) {
+            self::MANAGE => in_array('ROLE_ADMIN', $user->getRoles()),
+            default => throw new \LogicException('This code should not be reached!')
+        };
+    }
+}
